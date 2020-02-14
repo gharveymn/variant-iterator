c85cffe6499280dcf291c38fdab05d477b93d2c6
Gene Harvey <gharveymn@gmail.com>
2020-02-13 18:43:40 -0600

    move helper functions

--- source/include/variant-iterator.hpp
+++ source/include/variant-iterator.hpp
@@ -18,66 +18,206 @@
 namespace gch
 {
 
-  namespace detail
+  struct void_it_t
   {
-    template <typename T, typename ...Ts>
-    struct all_same : std::conjunction<std::is_same<T, Ts>...>
-    { };
+    static constexpr struct engage_tag { } engage { };
+    constexpr explicit void_it_t (engage_tag) noexcept { }
+  };
 
-    template <std::size_t I, typename T, typename ...Ts>
-    struct select
-        : select<I - 1, Ts...>
-    { };
+  inline constexpr void_it_t void_it { void_it_t::engage };
 
-    template <typename T, typename ...Ts>
-    struct select<0, T, Ts...>
+  // iterator which doesn't actually do anything.
+  // This can be used as an end sentinel in variant_iterator.
+  template <typename Value>
+  class void_iterator
+  {
+  public:
+
+    class access_exception : public std::logic_error
     {
-      using type = T;
+    public:
+      access_exception (void)
+        : std::logic_error ("Tried to access a void iterator.")
+      { }
     };
 
-    template <std::size_t I, typename T, typename Head, typename ...Tail>
-    struct index : detail::index<I + 1, T, Tail...>
-    { };
+    class modification_exception : public std::logic_error
+    {
+    public:
+      modification_exception (void)
+        : std::logic_error ("Tried to modify a void iterator.")
+      { }
+    };
 
-    template <std::size_t I, typename T, typename ...Rest>
-    struct index<I, T, T, Rest...>
-        : std::integral_constant<std::size_t, I>
-    { };
+    using difference_type   = int8_t;
+    using value_type        = std::remove_cv_t<Value>;
+    using pointer           = Value *;
+    using reference         = Value&;
+    using iterator_category = std::random_access_iterator_tag;
 
-    template <std::size_t I, typename T>
-    struct index<I, T, T>
-        : std::integral_constant<std::size_t, I>
-    { };
+    void_iterator            (void)                     = default;
+    void_iterator            (const void_iterator&)     = default;
+    void_iterator            (void_iterator&&) noexcept = default;
+    void_iterator& operator= (const void_iterator&)     = default;
+    void_iterator& operator= (void_iterator&&) noexcept = default;
+    ~void_iterator           (void)                     = default;
 
-    template <std::size_t I, typename T, typename U>
-    struct index<I, T, U>
-    { };
+    constexpr /* implicit */ void_iterator (void_it_t) noexcept { };
 
-    template <typename T, typename = void, typename ...Ts>
-    struct is_element : std::false_type
-    { };
+    template <typename NonConst,
+      typename = std::enable_if_t<std::is_same_v<std::remove_const_t<Value>, NonConst>>>
+    constexpr /* implicit */ void_iterator (const void_iterator<NonConst> it) noexcept { }
 
-    template <typename T, typename ...Ts>
-    struct is_element<T, std::void_t<typename index<0, T, Ts...>::type>,
-                      Ts...>
-        : std::true_type
-    { };
+    constexpr void_iterator& operator++ (void)
+    {
+      throw modification_exception { };
+    }
 
-    template <typename It, typename = void>
-    struct is_iterator : std::false_type
-    { };
+    constexpr void_iterator operator++ (int)
+    {
+      throw modification_exception { };
+    }
 
-    template <typename It>
-    struct is_iterator<It, std::void_t<std::iterator_traits<It>>>
-        : std::true_type
-    { };
+    constexpr void_iterator& operator-- (void)
+    {
+      throw modification_exception { };
+    }
+
+    constexpr void_iterator operator-- (int)
+    {
+      throw modification_exception { };
+    }
+
+    constexpr void_iterator& operator+= (difference_type n)
+    {
+      throw modification_exception { };
+    }
+
+    [[nodiscard]]
+    constexpr void_iterator operator+ (difference_type n) const
+    {
+      if (n != 0)
+        throw std::logic_error { "cannot add non-zero integer to void_iterator type" };
+      return { };
+    }
+
+    constexpr void_iterator& operator-= (difference_type n)
+    {
+      throw modification_exception { };
+    }
+
+    [[nodiscard]]
+    constexpr void_iterator
+    operator- (difference_type n) const
+    {
+      return operator+ (-n);
+    }
+
+    [[nodiscard]]
+    constexpr difference_type
+    operator- (const void_iterator& other) const noexcept
+    {
+      return 0;
+    }
+
+    [[nodiscard]]
+    constexpr reference operator[] (difference_type n) const
+    {
+      throw access_exception { };
+    }
+
+    //
+    // The below functions will be wrong if other does not refer to the same
+    // object. However, this should be fine because it works the same way
+    // for std container iterators.
+    //
+
+    [[nodiscard]]
+    constexpr bool operator< (const void_iterator& other) const noexcept
+    {
+      return false;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator> (const void_iterator& other) const noexcept
+    {
+      return false;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator<= (const void_iterator& other) const noexcept
+    {
+      return true;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator>= (const void_iterator& other) const noexcept
+    {
+      return true;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator== (const void_iterator& other) const noexcept
+    {
+      return true;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator!= (const void_iterator& other) const noexcept
+    {
+      return false;
+    }
+
+    [[nodiscard]]
+    constexpr reference operator* (void) const
+    {
+      throw access_exception { };
+    }
+
+    [[nodiscard]]
+    constexpr pointer operator-> (void) const noexcept
+    {
+      throw access_exception { };
+    }
+
+  };
+
+  template <typename Value> [[nodiscard]]
+  constexpr void_iterator<Value>
+  operator+ (typename void_iterator<Value>::difference_type n, const void_iterator<Value>& it)
+  {
+    return it + n;
   }
+
+  template <typename Value>
+  using const_void_iterator = void_iterator<const Value>;
+
+  template <typename Value>
+  using reverse_void_iterator = std::reverse_iterator<void_iterator<Value>>;
+
+  template <typename Value>
+  using const_reverse_void_iterator = std::reverse_iterator<const_void_iterator<Value>>;
+
+  template <typename Value> using void_iter   = void_iterator<Value>;
+  template <typename Value> using void_citer  = const_void_iterator<Value>;
+  template <typename Value> using void_riter  = reverse_void_iterator<Value>;
+  template <typename Value> using void_criter = const_reverse_void_iterator<Value>;
 
   // basically just an iterator-compatible wrapper for a pointer
   // to avoid UB of incrementing a non-associated pointer.
   template <typename Value>
   class value_iterator
   {
+
+    template <typename ...>
+    struct not_self : std::true_type { };
+
+    template <typename ...Ts>
+    struct not_self<value_iterator<Ts...>> : std::false_type { };
+
+    template <typename T>
+    static constexpr bool not_self_v = not_self<T>::value;
+
   public:
 
     using difference_type   = int8_t;
@@ -88,6 +228,7 @@ namespace gch
 
     class dereference_exception : public std::exception
     {
+    public:
       [[nodiscard]]
       const char* what (void) const noexcept override
       {
@@ -104,11 +245,31 @@ namespace gch
     value_iterator& operator= (value_iterator&&) noexcept = default;
     ~value_iterator           (void)                      = default;
 
+    template <typename U = value_type,
+      std::enable_if_t<std::conjunction_v<
+        not_self<std::remove_reference_t<U>>,
+        std::is_constructible<value_type, U&&>,
+        std::is_convertible<U&&, value_type>>, bool> = true>
+    constexpr /* implicit */ value_iterator (U&& value)
+      : m_value (std::forward<U> (value)),
+        m_is_end (false)
+    { }
+
+    template <typename U = value_type,
+      std::enable_if_t<std::conjunction_v<
+        not_self<std::remove_reference_t<U>>,
+        std::is_constructible<value_type, U&&>,
+        std::negation<std::is_convertible<U&&, value_type>>>, bool> = false>
+    constexpr explicit value_iterator (U&& value)
+      : m_value (std::forward<U> (value)),
+        m_is_end (false)
+    { }
+
     template <typename ...Args,
               typename = std::enable_if_t<
-                std::is_constructible<value_type, Args...>::value>>
-    constexpr explicit value_iterator (Args&&... args)
-      : m_value (std::forward<Args> (args)...),
+                std::is_constructible<value_type, Args&&...>::value>>
+    constexpr explicit value_iterator (std::in_place_t, Args&&... args)
+      : m_value  (std::forward<Args> (args)...),
         m_is_end (false)
     { }
 
@@ -296,36 +457,21 @@ namespace gch
   using const_value_iterator = value_iterator<const Value>;
 
   template <typename Value>
-  using reverse_value_iterator
-    = std::reverse_iterator<value_iterator<Value>>;
+  using reverse_value_iterator = std::reverse_iterator<value_iterator<Value>>;
 
   template <typename Value>
-  using const_reverse_value_iterator
-    = std::reverse_iterator<const_value_iterator<Value>>;
+  using const_reverse_value_iterator = std::reverse_iterator<const_value_iterator<Value>>;
 
-  template <typename Value>
-  using value_iter   = value_iterator<Value>;
-
-  template <typename Value>
-  using value_citer  = const_value_iterator<Value>;
-
-  template <typename Value>
-  using value_riter  = reverse_value_iterator<Value>;
-
-  template <typename Value>
-  using value_criter = const_reverse_value_iterator<Value>;
+  template <typename Value> using value_iter   = value_iterator<Value>;
+  template <typename Value> using value_citer  = const_value_iterator<Value>;
+  template <typename Value> using value_riter  = reverse_value_iterator<Value>;
+  template <typename Value> using value_criter = const_reverse_value_iterator<Value>;
 
   // static_assert(std::is_trivially_default_constructible<value_iter<void *>>::value);
-  static_assert(std::is_trivially_copy_constructible<value_iter<void *>>::value);
-  static_assert(std::is_trivially_move_constructible<value_iter<void *>>::value);
-  static_assert(std::is_trivially_copy_assignable<value_iter<void *>>::value);
-  static_assert(std::is_trivially_move_assignable<value_iter<void *>>::value);
+  static_assert(std::is_trivially_copyable<value_iter<void *>>::value);
   static_assert(std::is_trivially_destructible<value_iter<void *>>::value);
 
-  static_assert(std::is_trivially_copy_constructible<value_citer<void *>>::value);
-  static_assert(std::is_trivially_move_constructible<value_citer<void *>>::value);
-  static_assert(std::is_trivially_copy_assignable<value_citer<void *>>::value);
-  static_assert(std::is_trivially_move_assignable<value_citer<void *>>::value);
+  static_assert(std::is_trivially_copyable<value_citer<void *>>::value);
   static_assert(std::is_trivially_destructible<value_citer<void *>>::value);
 
   template <typename T>
@@ -352,24 +498,80 @@ namespace gch
   template <typename ...Its>
   class variant_iterator
   {
+    template <typename T, typename ...Ts>
+    struct all_same : std::conjunction<std::is_same<T, Ts>...>
+    { };
+
+    template <std::size_t I, typename T, typename ...Ts>
+    struct select
+      : select<I - 1, Ts...>
+    { };
+
+    template <typename T, typename ...Ts>
+    struct select<0, T, Ts...>
+    {
+      using type = T;
+    };
+
+    template <std::size_t I, typename T, typename Head, typename ...Tail>
+    struct index : index<I + 1, T, Tail...>
+    { };
+
+    template <std::size_t I, typename T, typename ...Rest>
+    struct index<I, T, T, Rest...>
+      : std::integral_constant<std::size_t, I>
+    { };
+
+    template <std::size_t I, typename T>
+    struct index<I, T, T>
+      : std::integral_constant<std::size_t, I>
+    { };
+
+    template <std::size_t I, typename T, typename U>
+    struct index<I, T, U>
+    { };
+
+    template <typename T, typename = void, typename ...Ts>
+    struct is_element_impl : std::false_type
+    { };
+
+    template <typename T, typename ...Ts>
+    struct is_element_impl<T, std::void_t<typename index<0, T, Ts...>::type>, Ts...>
+      : std::true_type
+    { };
 
     template <std::size_t I, typename ...Ts>
-    using select_t    = typename detail::select<I, Ts...>::type;
+    using select_t    = typename select<I, Ts...>::type;
 
     template <typename T, typename ...Ts>
-    using is_element  = detail::is_element<T, void, Ts...>;
+    using is_element  = is_element_impl<T, void, Ts...>;
+
+    template <typename It, typename = void>
+    struct is_iterator : std::false_type
+    { };
 
     template <typename It>
-    using is_iterator = detail::is_iterator<It>;
+    struct is_iterator<It, std::void_t<std::iterator_traits<It>>>
+      : std::true_type
+    { };
 
     template <typename ...Ts>
-    static constexpr bool        all_same_v    = detail::all_same<Ts...>::value;
+    static constexpr bool        all_same_v    = all_same<Ts...>::value;
 
     template <typename T, typename ...Ts>
-    static constexpr std::size_t is_element_v  = detail::is_element<T, void, Ts...>::value;
+    static constexpr std::size_t is_element_v  = is_element<T, Ts...>::value;
 
     template <typename It>
-    static constexpr bool        is_iterator_v = detail::is_iterator<It>::value;
+    static constexpr bool        is_iterator_v = is_iterator<It>::value;
+
+    template <typename ...>
+    struct not_self : std::true_type { };
+
+    template <typename ...Ts>
+    struct not_self<variant_iterator<Ts...>> : std::false_type { };
+
+    template <typename T>
+    static constexpr bool not_self_v = not_self<T>::value;
 
     static_assert (std::conjunction_v<is_iterator<Its>...>,
                    "All template arguments must be iterators.");
@@ -411,8 +613,9 @@ namespace gch
     using const_reference   = const value_type&;
     using iterator_category = std::common_type_t<category_t<Its>...>;
 
-    struct type_exception : std::exception
+    class type_exception : public std::exception
     {
+    public:
       [[nodiscard]]
       const char* what (void) const noexcept override
       {
@@ -466,72 +669,71 @@ namespace gch
     ~variant_iterator           (void)                        = default;
 
     // value copying initializer
-    template <typename It,
-              typename = std::enable_if_t<is_element_v<It, Its...>>>
-    constexpr explicit variant_iterator (const It& it)
-        : m_variant (it)
+//    template <typename It,
+//              typename = std::enable_if_t<is_element_v<It, Its...>>>
+//    constexpr explicit variant_iterator (const It& it)
+//        : m_variant (it)
+//    { }
+
+    template <typename T,
+              typename = std::enable_if_t<not_self_v<std::decay_t<T>>>>
+    constexpr /* implicit */ variant_iterator (T&& val)
+      : m_variant (std::forward<T> (val))
     { }
 
-    template <typename It,
-              typename = std::enable_if_t<is_element_v<It, Its...>>>
-    constexpr explicit variant_iterator (It&& it)
-      : m_variant (std::forward<It> (it))
-    { }
-
-    constexpr variant_iterator& operator++ (void) noexcept
+    constexpr variant_iterator& operator++ (void)
     {
       std::visit ([] (auto&& it) -> void { ++it; }, m_variant);
       return *this;
     }
 
-    constexpr variant_iterator operator++ (int) noexcept
+    constexpr variant_iterator operator++ (int)
     {
       return std::visit ([] (auto&& it) -> variant_iterator
                          { return variant_iterator (it++); }, m_variant);
     }
 
-    constexpr variant_iterator& operator-- (void) noexcept
+    constexpr variant_iterator& operator-- (void)
     {
       std::visit ([] (auto&& it) -> void { --it; }, m_variant);
       return *this;
     }
 
-    constexpr variant_iterator operator-- (int) noexcept
+    constexpr variant_iterator operator-- (int)
     {
       return std::visit ([] (auto&& it) -> variant_iterator
                          { return variant_iterator (it--); }, m_variant);
     }
 
-    constexpr variant_iterator& operator+= (difference_type n) noexcept
+    constexpr variant_iterator& operator+= (difference_type n)
     {
       std::visit ([&n] (auto&& it) { it += n; }, m_variant);
       return *this;
     }
 
     [[nodiscard]]
-    constexpr variant_iterator operator+ (difference_type n) const noexcept
+    constexpr variant_iterator operator+ (difference_type n) const
     {
       return std::visit ([&n] (auto&& it) -> variant_iterator
                          { return variant_iterator (it + n); }, m_variant);
     }
 
     [[nodiscard]]
-    friend constexpr variant_iterator operator+ (difference_type n,
-                                       const variant_iterator& var) noexcept
+    friend constexpr variant_iterator operator+ (difference_type n, const variant_iterator& var)
     {
       return std::visit ([&n] (auto&& it) -> variant_iterator
                          { return variant_iterator<Its...> (n + it); },
                          var.m_variant);
     }
 
-    constexpr variant_iterator& operator-= (difference_type n) noexcept
+    constexpr variant_iterator& operator-= (difference_type n)
     {
       std::visit ([&n] (auto&& it) -> void { it -= n; }, m_variant);
       return *this;
     }
 
     [[nodiscard]]
-    constexpr variant_iterator operator- (difference_type n) const noexcept
+    constexpr variant_iterator operator- (difference_type n) const
     {
       return std::visit ([&n] (auto&& it) -> variant_iterator
                          { return variant_iterator (it - n); }, m_variant);
